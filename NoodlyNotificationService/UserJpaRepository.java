664e97622c08aad73f126ef1ae6385d4a30a934b
tlbluejay <47327939+tlbluejay@users.noreply.github.com>
2021-04-25 23:49:44 -0600

    Delete NoodlyNotificationService directory

--- NoodlyNotificationService/UserJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package lewis.trenton.NoodlyNotificationService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserJpaRepository extends JpaRepository<User, Integer>{
-
-}
