f063a39cffa79fc7f01c85850d7870bb05e4a77c
tlbluejay <47327939+tlbluejay@users.noreply.github.com>
2021-04-25 23:53:22 -0600

    Delete NoodlyUserService directory

--- NoodlyUserService/UserJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package lewis.trenton.NoodlyUserService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserJpaRepository extends JpaRepository<User, Long>{
-	
-	public User findByEmail(String email);
-
-}
